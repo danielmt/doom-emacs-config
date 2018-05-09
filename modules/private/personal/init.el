bf2082904f87e73ccedcf027ba8df484367cdb27
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-05-09 14:21:41 +0100

    fix home/end

--- modules/private/personal/init.el
+++ modules/private/personal/init.el
@@ -3,6 +3,6 @@
 (setq doom-font (font-spec :family "PragmataPro Mono" :size 18))
 
 ; fix home/end on mac
-(global-set-key (kbd "<home>") 'evil-beginning-of-line)
-(global-set-key (kbd "<end>") 'evil-end-of-line)
+(global-set-key (kbd "<home>") 'doom/backward-to-bol-or-indent)
+(global-set-key (kbd "<end>") 'doom/forward-to-last-non-comment-or-eol)
 
