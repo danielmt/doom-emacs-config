e811516a0d883ad28d270eee4ee1245848f50616
Daniel Tavares <daniel.mts@gmail.com>
2018-05-09 17:34:14 +0100

    cleanup

--- modules/private/completion/config.el
+++ modules/private/completion/config.el
@@ -1,7 +1,5 @@
 (def-package! flx
   :config
-  ;(setq ivy-re-builders-alist
-  ;    '((t . ivy--regex-fuzzy)))
   (setq ivy-re-builders-alist
         '((ivy-switch-buffer . ivy--regex-plus)
           (t . ivy--regex-fuzzy)))
