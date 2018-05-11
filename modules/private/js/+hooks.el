a262a4388b322384552e7954730aebdfb0e2bf6c
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-05-11 13:32:36 +0100

    fix js indenting

--- modules/private/js/+hooks.el
+++ modules/private/js/+hooks.el
@@ -1,4 +1,16 @@
-; highlight current code identifier
-
 (add-hook! 'rjsx-mode-hook
-  (tide-hl-identifier-mode +1))
+  (setq
+   js2-mode-show-strict-warnings nil
+   js2-mode-show-parse-errors nil
+   js-indent-level 2
+   js2-basic-offset 2
+   js2-strict-trailing-comma-warning nil
+   js2-strict-missing-semi-warning nil
+
+   tide-format-options '(:indentSize 2 :indentStyle 2 :tabSize 2 :ConvertTabsToSpaces t)
+  ; tide-tsserver-executable "/usr/local/bin/tsserver"
+  )
+
+  ; highlight current code identifier
+  (tide-hl-identifier-mode +1)
+  )
