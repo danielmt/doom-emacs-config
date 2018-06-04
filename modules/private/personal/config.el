cf80cf2b29ecd6b7345a95c1dd95b19ec74b2d2d
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-06-04 11:56:51 +0100

    adding fonts/images to projectile ignore

--- modules/private/personal/config.el
+++ modules/private/personal/config.el
@@ -1,2 +1,11 @@
 (after! projectile
-  (add-to-list 'projectile-globally-ignored-file-suffixes ".snap"))
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".snap")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".ttf")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".eot")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".woff")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".woff2")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".svg")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".png")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".jpg")
+  (add-to-list 'projectile-globally-ignored-file-suffixes ".gif")
+  )
