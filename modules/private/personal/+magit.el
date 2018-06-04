a9e3684e10b5dff0babae28cdb0037835d9dd394
Daniel Tavares <daniel.mts@gmail.com>
2018-06-04 14:29:24 +0100

    adding magit customizations

---
+++ modules/private/personal/+magit.el
@@ -0,0 +1,16 @@
+(setq
+  magit-push-always-verify nil
+  magit-popup-show-common-commands nil
+  magit-auto-revert-mode t
+  magit-revert-buffers 1
+  magit-commit-show-diff nil
+  magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1
+  magit-diff-refine-hunk 'all
+  magit-delete-by-moving-to-trash nil
+  magit-revision-use-gravatar-kludge t)
+
+(after! magit
+  (remove-hook 'git-commit-finish-query-functions
+               'git-commit-check-style-conventions)
+  (remove-hook 'server-switch-hook
+               'magit-commit-diff))
