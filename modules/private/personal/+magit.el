6d41cd6e17f23503df5100a40dec5b551954df80
Daniel Tavares <daniel.mts@gmail.com>
2018-06-06 11:54:08 +0100

    fix commit length

--- modules/private/personal/+magit.el
+++ modules/private/personal/+magit.el
@@ -4,13 +4,16 @@
   magit-auto-revert-mode t
   magit-revert-buffers 1
   magit-commit-show-diff nil
-  git-commit-summary-max-length 140
   magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1
   magit-diff-refine-hunk 'all
   magit-delete-by-moving-to-trash nil
   magit-revision-use-gravatar-kludge t)
 
 (after! magit
+  (setq
+    git-commit-summary-max-length 999
+    git-commit-fill-column 999)
+
   (remove-hook 'git-commit-finish-query-functions
                'git-commit-check-style-conventions)
   (remove-hook 'server-switch-hook
