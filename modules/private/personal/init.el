24c89df041b74db75f75bd4d8f4e5a756a5fc713
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-06-12 14:39:10 +0100

    M-RET: toggle fs

--- modules/private/personal/init.el
+++ modules/private/personal/init.el
@@ -5,6 +5,7 @@
 ; fix home/end on mac
 (global-set-key (kbd "<home>") 'doom/backward-to-bol-or-indent)
 (global-set-key (kbd "<end>") 'doom/forward-to-last-non-comment-or-eol)
+(global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
 
 ; dark titlebar on mac
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
