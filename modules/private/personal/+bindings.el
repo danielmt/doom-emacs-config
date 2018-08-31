7cd2ebbbd74be3ef535bc44c6eda3ea1c7de29c7
Daniel Tavares <daniel.mts@gmail.com>
2018-08-31 02:00:11 +0100

    fix: missing bindings

---
+++ modules/private/personal/+bindings.el
@@ -0,0 +1,4 @@
+; fix home/end
+(global-set-key (kbd "<home>") 'doom/backward-to-bol-or-indent)
+(global-set-key (kbd "<end>") 'doom/forward-to-last-non-comment-or-eol)
+(global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
