32d627f96120ba0c62a37e3e4fd0d4223f3f35c4
Daniel Mascarenhas <daniel.mts@gmail.com>
2018-09-13 22:54:03 +0100

    switch font to pragmata with ligatures and enable its prettify symbols

--- modules/private/personal/+pragmata.el
+++ modules/private/personal/+pragmata.el
@@ -227,8 +227,13 @@
             ("[["        #XEA80)
             ("]]"        #XEA81)
             ("\">"       #XEA90)
+            ("_|_"       #XEA97)
             )))
 
 (defun add-pragmatapro-prettify-symbols-alist ()
   (dolist (alias pragmatapro-prettify-symbols-alist)
     (push alias prettify-symbols-alist)))
+
+(add-hook! 'prog-mode-hook #'add-pragmatapro-prettify-symbols-alist)
+
+(global-prettify-symbols-mode t)
