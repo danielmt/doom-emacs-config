1728e5a5cdf190f6ed4be2d97aa442e71c7fb561
Daniel Tavares <daniel.mts@gmail.com>
2019-01-31 13:32:31 +0000

    additional smartparens bindings + cleanup of projectile settings

--- modules/private/personal/+bindings.el
+++ modules/private/personal/+bindings.el
@@ -1,4 +1,39 @@
-; fix home/end
-(global-set-key (kbd "<home>") 'doom/backward-to-bol-or-indent)
-(global-set-key (kbd "<end>") 'doom/forward-to-last-non-comment-or-eol)
-(global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
+(map!
+ [home]     #'doom/backward-to-bol-or-indent
+ [end]      #'doom/forward-to-last-non-comment-or-eol
+
+ ;; fix this, not working after general.el rewrite
+ [M-return] #'toggle-frame-fullscreen
+
+ (:after smartparens
+   (:map smartparens-mode-map
+     [s-down]  #'sp-down-sexp
+     [s-up]    #'sp-up-sexp
+     [C-left]  #'sp-beginning-of-sexp
+     [C-right] #'sp-end-of-sexp
+     [M-up]    #'sp-backward-symbol
+     [M-down]  #'sp-forward-symbol
+
+    ;; FIXME single-quote, underscore and angle-bracket not working
+     "C-c ("   #'wrap-with-parens
+     "C-c ["   #'wrap-with-brackets
+     "C-c {"   #'wrap-with-braces
+     "C-c '"   #'wrap-with-single-quotes
+     "C-c \""  #'wrap-with-double-quotes
+     "C-c `"   #'wrap-with-back-quotes
+     "C-c _"   #'wrap-with-underscores
+     "C-c <"   #'wrap-with-angle-brackets)))
+
+ ; (:after smartparens
+ ;   (:map smartparens-mode-map
+ ;     "C-M-a"     #'sp-beginning-of-sexp
+ ;     "C-M-e"     #'sp-end-of-sexp
+ ;     "C-M-f"     #'sp-forward-sexp
+ ;     "C-M-b"     #'sp-backward-sexp
+ ;     "C-M-d"     #'sp-splice-sexp
+ ;     "C-M-k"     #'sp-kill-sexp
+ ;     "C-M-t"     #'sp-transpose-sexp
+ ;     "C-<right>" #'sp-forward-slurp-sexp
+ ;     "M-<right>" #'sp-forward-barf-sexp
+ ;     "C-<left>"  #'sp-backward-slurp-sexp
+ ;     "M-<left>"  #'sp-backward-barf-sexp))
