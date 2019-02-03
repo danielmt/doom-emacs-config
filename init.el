9703f03d8b2f78493fd5e96a2d94825d315855cd
Daniel Tavares <daniel.mts@gmail.com>
2019-02-03 14:13:45 +0000

    reenable rust

--- init.el
+++ init.el
@@ -126,7 +126,7 @@
        ;;racket            ; a DSL for DSLs
        rest              ; Emacs as a REST client
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        sh                ; she sells (ba|z|fi)sh shells on the C xor (+fish)
        ;;solidity          ; do you need a blockchain? No.
