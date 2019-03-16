7a09a6573b7ff55e0c64e2f02df16ee5a2d25255
Daniel Mascarenhas <daniel.mts@gmail.com>
2019-03-16 17:45:30 +0000

    cleaning up and letting font size be set automatically on linux

--- modules/private/personal/init.el
+++ modules/private/personal/init.el
@@ -1,24 +1,31 @@
 ; remember to install font icons:
 ; M-x all-the-icons-install-fonts
-(setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 18))
-(setq doom-theme 'doom-spacegrey)
 
-(setq shell-file-name "zsh")
+(if (memq window-system '(ns mac))
+  ; mac customizations
+  (progn
+    (setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 18))
 
-; no line numbers
-(setq doom-line-numbers-style nil)
+    ; dark titlebar
+    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+    (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
-; mac customizations
-(when (memq window-system '(ns mac))
-  ; dark titlebar
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . dark))
+    (setq
+      mac-option-key-is-meta t
+      mac-command-key-is-meta nil
+      mac-command-modifier 'super
+      mac-option-modifier 'meta))
+  (setq doom-font (font-spec :family "PragmataPro Mono Liga"))
+  )
 
-  (setq
-    mac-option-key-is-meta t
-    mac-command-key-is-meta nil
-    mac-command-modifier 'super
-    mac-option-modifier 'meta)
+(setq
+  doom-theme 'doom-spacegrey
+
+  ; fix paths that are only set on zsh settings
+  shell-file-name "zsh"
+
+  ; no line numbers
+  doom-line-numbers-style nil
   )
 
 (load! "+magit")
