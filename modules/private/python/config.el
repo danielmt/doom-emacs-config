c9117e1962fc393c23de52ac81f32307aff17b62
Daniel Tavares <daniel.mts@gmail.com>
2019-03-21 23:50:33 +0000

    fix: feature/syntax-checker -> tools/flycheck

--- modules/private/python/config.el
+++ modules/private/python/config.el
@@ -1,6 +1,6 @@
 (def-package! flycheck-mypy
   :when (featurep! :lang python)
-  :when (featurep! :feature syntax-checker)
+  :when (featurep! :tools flycheck)
   :hook (python-mode . flycheck-mode)
   :config
   (add-hook! 'python-mode
