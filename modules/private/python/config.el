48abee29efe394e9c46bb31eeb6d58a3438dba02
Daniel Tavares <daniel.tavares@corp.badoo.com>
2019-04-09 09:55:16 +0100

    mypy checker working through flake8+flake8-mypy

--- modules/private/python/config.el
+++
@@ -1,8 +0,0 @@
-(def-package! flycheck-mypy
-  :when (featurep! :lang python)
-  :when (featurep! :tools flycheck)
-  :hook (python-mode . flycheck-mode)
-  :config
-  (add-hook! 'python-mode
-             (setq flycheck-checker 'python-mypy))
-  )
