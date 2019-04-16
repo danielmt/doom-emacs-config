0c3c1997d338243de4691d5ce2c50f4b4a56b72d
Daniel Mascarenhas <daniel.mts@gmail.com>
2019-04-16 13:58:05 +0100

    additional smartparens bindings and move C-left/right to M-left/right

--- modules/private/personal/+bindings.el
+++ modules/private/personal/+bindings.el
@@ -7,10 +7,17 @@
 
  (:after smartparens
    (:map smartparens-mode-map
+     [C-M-a]   #'sp-beginning-of-sexp
+     [C-M-e]   #'sp-end-of-sexp
+     [C-M-f]   #'sp-forward-sexp
+     [C-M-b]   #'sp-backward-sexp
+     [C-M-k]   #'sp-kill-sexp
+     [C-M-t]   #'sp-transpose-sexp
+
      [s-down]  #'sp-down-sexp
      [s-up]    #'sp-up-sexp
-     [C-left]  #'sp-beginning-of-sexp
-     [C-right] #'sp-end-of-sexp
+     [M-left]  #'sp-beginning-of-sexp
+     [M-right] #'sp-end-of-sexp
      [M-up]    #'sp-backward-symbol
      [M-down]  #'sp-forward-symbol
 
@@ -26,13 +33,7 @@
 
  ; (:after smartparens
  ;   (:map smartparens-mode-map
- ;     "C-M-a"     #'sp-beginning-of-sexp
- ;     "C-M-e"     #'sp-end-of-sexp
- ;     "C-M-f"     #'sp-forward-sexp
- ;     "C-M-b"     #'sp-backward-sexp
  ;     "C-M-d"     #'sp-splice-sexp
- ;     "C-M-k"     #'sp-kill-sexp
- ;     "C-M-t"     #'sp-transpose-sexp
  ;     "C-<right>" #'sp-forward-slurp-sexp
  ;     "M-<right>" #'sp-forward-barf-sexp
  ;     "C-<left>"  #'sp-backward-slurp-sexp
