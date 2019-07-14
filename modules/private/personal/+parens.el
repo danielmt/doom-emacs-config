3acc462f02d4380a3053de350b8e5ad0433c984c
Daniel Tavares <daniel.mts@gmail.com>
2019-07-14 19:43:02 +0100

    use sp-pair directly

--- modules/private/personal/+parens.el
+++ modules/private/personal/+parens.el
@@ -1,29 +1,11 @@
-(defmacro def-pairs (pairs)
-  "Define functions for pairing. PAIRS is an alist of (NAME . STRING)
-conses, where NAME is the function name that will be created and
-STRING is a single-character string that marks the opening character.
-
-  (def-pairs ((paren . \"(\")
-              (bracket . \"[\"))
-
-defines the functions WRAP-WITH-PAREN and WRAP-WITH-BRACKET,
-respectively."
-  `(progn
-     ,@(loop for (key . val) in pairs
-             collect
-             `(defun ,(read (concat
-                             "wrap-with-"
-                             (prin1-to-string key)
-                             "s"))
-                  (&optional arg)
-                (interactive "p")
-                (sp-wrap-with-pair ,val)))))
-
-(def-pairs ((paren         . "(")
-            (bracket       . "[")
-            (brace         . "{")
-            (single-quote  . "'")
-            (double-quote  . "\"")
-            (back-quote    . "`")
-            (underscore    . "_")
-            (angle-bracket . "<")))
+(after! smartparens
+  (sp-pair "(" ")" :wrap "C-c (")
+  (sp-pair "[" "]" :wrap "C-c [")
+  (sp-pair "\{" "\}" :wrap "C-c \{")
+  (sp-pair "<" ">" :wrap "C-c <")
+  (sp-pair "_" "_" :wrap "C-c _")
+  (sp-pair "*" "*" :wrap "C-c *")
+  (sp-pair "\"" "\"" :wrap "C-c \"")
+  (sp-pair "`" "`" :wrap "C-c `")
+  (sp-pair "\'" "\'" :wrap "C-c \'") ; conflicts with markdown
+  )
