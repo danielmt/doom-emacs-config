63497d418d75d88c5382fd969f829987efcde17c
Daniel Tavares <daniel.mts@gmail.com>
2019-08-02 23:15:03 +0100

    fix deft config

--- modules/private/gtd/config.el
+++ modules/private/gtd/config.el
@@ -1,15 +1,13 @@
-(after! deft
-  (setq
-    deft-directory "~/Tresor/notes"
-    deft-recursive t
-    deft-extensions '("md" "org" "txt")
-    deft-default-extension "md"
-    deft-markdown-mode-title-level 1
-    deft-use-filename-as-title t
-    deft-use-filter-string-for-filename t
-    deft-file-naming-rules
-      '((noslash . "-")
-      (nospace . "-")
-      (case-fn . downcase))
-    )
+(setq
+  deft-directory "~/Tresor/notes"
+  deft-recursive t
+  deft-extensions '("md" "org" "txt")
+  deft-default-extension "md"
+  deft-markdown-mode-title-level 1
+  deft-use-filename-as-title t
+  deft-use-filter-string-for-filename t
+  deft-file-naming-rules
+    '((noslash . "-")
+    (nospace . "-")
+    (case-fn . downcase))
   )
