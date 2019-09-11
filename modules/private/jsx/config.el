01e84b284083df72116a3678dd152f2f782764d1
Daniel Tavares <daniel.mts@gmail.com>
2019-09-11 11:08:20 +0100

    adds company-flow

--- modules/private/jsx/config.el
+++ modules/private/jsx/config.el
@@ -17,4 +17,26 @@
   (flycheck-add-next-checker 'javascript-flow 'javascript-eslint)
   )
 
+(defun flow/set-flow-executable ()
+  (interactive)
+  (let* ((os (pcase system-type
+               ('darwin "osx")
+               ('gnu/linux "linux64")
+               (_ nil)))
+         (root (locate-dominating-file  buffer-file-name  "node_modules/flow-bin"))
+         (executable (car (file-expand-wildcards
+                           (concat root "node_modules/flow-bin/*" os "*/flow")))))
+    (setq-local company-flow-executable executable)
+    ;; These are not necessary for this package, but a good idea if you use
+    ;; these other packages
+    (setq-local flow-minor-default-binary executable)
+    (setq-local flycheck-javascript-flow-executable executable)))
+
+(def-package! company-flow
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-flow)
+  (add-hook 'rjsx-mode-hook #'flow/set-flow-executable t)
+  )
+
 (load! "+hooks")
