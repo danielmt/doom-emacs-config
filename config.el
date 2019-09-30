a2dfac2693eeb8ff05749917240885e61ad3c002
Daniel Tavares <daniel.mts@gmail.com>
2019-09-30 11:08:34 +0100

    formatting

--- config.el
+++ config.el
@@ -1,31 +1,31 @@
 ;; MacOS specifics
 (if (memq window-system '(ns mac))
-  (progn
-    (setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 18))
+    (progn
+      (setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 18))
 
-    ; dark titlebar
-    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-    (add-to-list 'default-frame-alist '(ns-appearance . dark))
+      ; dark titlebar
+      (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+      (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
-    (setq
-      mac-option-key-is-meta t
-      mac-command-key-is-meta nil
-      mac-command-modifier 'super
-      mac-option-modifier 'meta)
-    )
+      (setq
+       mac-option-key-is-meta t
+       mac-command-key-is-meta nil
+       mac-command-modifier 'super
+       mac-option-modifier 'meta)
+      )
 
   ; Linux will pickup best font size depending on resolution
   (setq
-    doom-font (font-spec :family "PragmataPro Mono Liga"))
+   doom-font (font-spec :family "PragmataPro Mono Liga"))
   )
 
 (setq
-  doom-theme 'doom-spacegrey
+ doom-theme 'doom-palenight
 
-  ; no line numbers
-  doom-line-numbers-style nil
+ ; no line numbers
+ doom-line-numbers-style nil
 
-  ; don't kill the buffer if it's removed from every(or non weak) perspective.
-  persp-autokill-buffer-on-remove nil
+ ; don't kill the buffer if it's removed from every(or non weak) perspective.
+ persp-autokill-buffer-on-remove nil
 
-  shell-file-name "zsh")
+ shell-file-name "zsh")
