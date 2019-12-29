388a59ee182aa63047e9472d8cd4377a91674582
Daniel Tavares <daniel.mts@gmail.com>
2019-12-29 10:07:39 +0000

    sync to develop

--- init.el
+++ init.el
@@ -50,6 +50,7 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
+       ;;zen               ; distraction-free coding or writing
 
        :editor
        (evil             ; come to the dark side, we have cookies
