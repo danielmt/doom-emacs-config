2b9d47c0dce3ef0babbc8c8bd16201264710d803
Daniel Tavares <daniel.mts@gmail.com>
2021-05-25 18:16:03 +0100

    mode projectile config to its own file

---
+++ modules/private/personal/+projectile.el
@@ -0,0 +1,4 @@
+(after! projectile
+  (setq projectile-globally-ignored-file-suffixes
+        '(".elc" ".pyc" ".o" ".snap" ".ttf" ".eot" ".woff" ".woff2" ".svg" ".png" ".jpg" ".gif"))
+  )
--- modules/private/personal/config.el
+++ modules/private/personal/config.el
@@ -1,9 +1,5 @@
-(after! projectile
-  (setq projectile-globally-ignored-file-suffixes
-        '(".elc" ".pyc" ".o" ".snap" ".ttf" ".eot" ".woff" ".woff2" ".svg" ".png" ".jpg" ".gif"))
-  )
-
 (load! "+pragmata")
+(load! "+projectile")
 (load! "+parens")
 (load! "+bindings")
 (load! "+magit")
