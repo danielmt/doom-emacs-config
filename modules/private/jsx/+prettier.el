674b47e9f017c0d702302bca078094a99aeacbbe
Daniel Tavares <daniel.mts@gmail.com>
2021-05-25 18:21:58 +0100

    move prettier config to jsx/prettier

---
+++ modules/private/jsx/+prettier.el
@@ -0,0 +1,4 @@
+(use-package! prettier-js
+  :config
+  (add-hook 'rjsx-mode-hook 'prettier-js-mode)
+  )
--- modules/private/jsx/config.el
+++ modules/private/jsx/config.el
@@ -1,7 +1,3 @@
-(use-package! prettier-js
-  :config
-  (add-hook 'rjsx-mode-hook 'prettier-js-mode)
-  )
-
 ;(load! "+flow")
+(load! "+prettier")
 (load! "+hooks")
