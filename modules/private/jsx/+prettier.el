a529cb38ad53880d763b1c82c9978757b6d8f192
Daniel Tavares <daniel.mts@gmail.com>
2021-12-17 11:48:31 +0000

    enable prettier-js for typescript

--- modules/private/jsx/+prettier.el
+++ modules/private/jsx/+prettier.el
@@ -1,4 +1,6 @@
 (use-package! prettier-js
   :config
   (add-hook 'rjsx-mode-hook 'prettier-js-mode)
+  (add-hook 'typescript-tsx-mode-hook 'prettier-js-mode)
+  (add-hook 'typescript-mode-hook 'prettier-js-mode)
   )
